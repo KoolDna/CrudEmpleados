d5efed4c54d1b805db216081e7458ba69e2dc889
mad <mario.a.delgadillo@nube.unadmexico.mx>
2021-12-03 15:57:18 -0600

    2nd Commit

--- editar.php
+++ editar.php
@@ -16,13 +16,27 @@
 <body>
   <header>
     <div class="container">      
-      <h1>Movimientos a Empleados</h1>
+      <h1>Modificar Empleados</h1>
     </div>
   </header>
+  <?php
+    include 'basededatos.php';
+  if (isset($_GET['modificar'])){
+        $id= $_GET['modificar'];
+        $resultado = consulta_id($id);        
+        if (mysqli_num_rows($resultado)==1){
+            $registro = mysqli_fetch_array($resultado);
+            $nombre = $registro['nombre'];
+            $edad = $registro['edad'];
+         }else{    
+            header('Location:index.php');
+            die(); }
+    }
 
+    ?>
   <div class="container">
     <div class="row justify-content-center">
-      <form method="POST" action="procesos.php">
+      <form method="POST" action="procesos.php?actualizar=<?php echo $id;?>">
         <div class="form-group">
           <label>Nombre del Empleado</label>
           <input id="empleado_nombre" class="form-control" type="text" name="empleado_nombre" value="<?php echo $nombre; ?>" placeholder="Ingresa el nombre del empleado">
@@ -32,36 +46,12 @@
           <input id="Empleado Edad" class="form-control" type="text" name="empleado_edad" value="<?php echo $edad;?>" placeholder="Ingresa la edad del empleado">
         </div>
         <div class="form-group">
-          <button class="btn btn-primary" type="submit" name="guardar">Guardar</button>
+          <button class="btn btn-info" type="submit" name="actualizar">Actualizar</button>
         </div>
       </form>
     </div>
   </div>
-  <div class="container">
-    <div class="row justify-content-center">
-      <table class="table">
-        <thead>
-          <tr>
-            <th>Nombre del empleado</th>
-            <th>Edad del empleado</th>
-            <th colspan="2">Acción</th>
-          </tr>
-        </thead>
 
-        <?php
-        include("basededatos.php");
-        $resultado = consulta_bd();
-        while ($registro = mysqli_fetch_assoc($resultado)) {
-          echo "<tr>";
-          echo "<td>" . $registro["nombre"] ."</td>" . "<td>". "(" . $registro["edad"] . " años)" . "</td>";
-          echo "<td>" . "<a href=editar.php?modificar=" . $registro["Id"] . " class = 'btn btn-info'>Modificar  </a></td>";
-          echo "<td>" . "<a href=procesos.php?eliminar=" . $registro["Id"] . " class = 'btn btn-danger'>Eliminar</a></td>";
-          echo "</tr>";
-        }
-        ?>
-      </table>
-    </div>
-  </div>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 
--- procesos.php
+++ procesos.php
@@ -22,6 +22,23 @@
             die(mysqli_connect_error());
         }        
     }
+    if (isset($_POST['actualizar'])){
+        $id= $_GET['actualizar'];
+        $Nombre = $_POST['empleado_nombre'];
+        $Edad = $_POST['empleado_edad'];
+        $consulta = "UPDATE empleados SET nombre='$Nombre', edad = '$Edad' WHERE Id = '$id'";
+        $resultado=mysqli_query($conexionbd,$consulta);
+        if ($resultado) {
+            echo "Empleado actualizado. <br />";
+            header('Location:index.php');
+            die();
+        } else {
+            echo "error en la ejecución de la consulta.".$id." <br />";    
+            die(mysqli_connect_error());
+        }        
+    }
+
+
     if (isset($_GET['eliminar'])){
         $id = $_GET['eliminar'];
         eliminar_e($id);       
@@ -32,10 +49,9 @@
     }
     if (isset($_GET['modificar'])){
         $id= $_GET['modificar'];
-        $resultado = consulta_id($id);
-        print_r($resultado);
+        $resultado = consulta_id($id);        
         if (mysqli_num_rows($resultado)==1){
-            $registro = mysqli_fetch_assoc($resultado);
+            $registro = mysqli_fetch_array($resultado);
             $nombre = $registro['nombre'];
             $edad = $registro['edad'];
             $update = true;
